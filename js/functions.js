b1fba84027d700a99297f88249599f6e349f741b
Torystan <tristan.lp@live.fr>
2022-02-05 22:26:34 +0100

    feat: fonction time pour afficher l'heure

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+
+function time() {
+	let d = new Date();
+	let s = d.getSeconds();
+	let m = d.getMinutes();
+	let h = d.getHours();
+	timeDisplay.textContent = ('0' + h).substr(-2) + ':' + ('0' + m).substr(-2) + ':' + ('0' + s).substr(-2);
+}
+
+setInterval(time, 1000);
